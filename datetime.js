f280d14bfda71fb845555c43b567bfac87845f79
Abdul quddos <abdulquddoshafiz@gmail.com>
2023-02-11 22:07:25 +0500

    Fifth commit

---
+++ datetime.js
@@ -0,0 +1,45 @@
+//date method (get and set)
+//date is created by four ways
+//date object is created by using new date() constructor 
+//time method (get and set)
+// let currDate = new Date()
+// console.log(currDate) 
+// console.log(currDate.toLocaleString()) 
+// console.log(currDate.toString())
+// console.log(currDate.toLocaleDateString())
+
+
+//DATE.now returns us milliseconds since janaury 1, 1970 00:00:00 UTC
+// console.log(Date.now())
+//there will be minimum two arguments year and month
+// var d = new Date(2022,3,5,20,20,45,0)
+// console.log(d.toLocaleString())
+
+// var d = new Date(1676111296288)
+// console.log(d.toLocaleString())
+
+// let currDate = new Date()
+// console.log(currDate.getDate())
+// console.log(currDate.getDay())
+// console.log(currDate.getFullYear())
+// console.log(currDate.getHours())
+
+//time method
+// const currTime= new Date()
+// console.log(currTime.getHours())
+// console.log(currTime.getMinutes())
+// console.log(currTime.getSeconds())
+// console.log(currTime.getTime())
+
+//settime()
+// console.log(currTime.setTime())
+// console.log(currTime.setHours(5))
+// console.log(currTime.setMinutes(5))
+// console.log(currTime.setSeconds(10))
+// console.log(currTime.setMilliseconds(100))
+
+
+//practice time
+// console.log(new Date().toLocaleDateString())
+// console.log(new Date().toLocaleTimeString())
+// console.log(new Date().toLocaleString())
---
+++ mathobject.js
@@ -0,0 +1,39 @@
+//math object is used to perform mathematical tasks on computer
+// console.log(Math.PI)
+
+//math.round()
+// let num = 10.2543
+// console.log(Math.round(num))
+
+//math.pow
+// console.log(Math.pow(2,5))
+
+
+//Math.sqrt()
+// console.log(Math.sqrt(36))
+
+//Math.abs()
+//return negative value to the positive
+// console.log(Math.abs(-4.4343))
+
+//Math.ceil() returns the value of x rounded up to its nearest integer
+// console.log(Math.ceil(4.1))
+
+//Math.floor() return the value of x rounded up to whole number of itself
+// console.log(Math.floor(4.9))
+// console.log(Math.floor(4.4))
+
+
+//Max()
+// console.log(Math.max(3,4,1,8))
+
+//Min()
+// console.log(Math.min(2,4,6,9,10))
+
+//Math.random()
+// console.log(Math.floor(Math.random()*10))
+
+
+//Math.trunc()
+//It returns the integer part of a number
+// console.log(Math.trunc(-99.8))
